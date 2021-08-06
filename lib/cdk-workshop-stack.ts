4038d7e4349a8ce7560acb803e9a3b305caad9a1
Jacky Price <the.jacky.price@googlemail.com>
2021-08-06 18:33:49 +0100

    add api gateway to trigger lambda

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,20 +1,19 @@
 import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda'
-
-// https://cdkworkshop.com/20-typescript.html
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler'
-    }
-    )
+      runtime: lambda.Runtime.NODEJS_14_X,    
+      code: lambda.Code.fromAsset('lambda'),  
+      handler: 'hello.handler'                
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
   }
 }
-
-
-
